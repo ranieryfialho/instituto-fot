beae4f51343d50c358b42b8ed66e6cc08ac03bdf
ranieryfialho <ranieryfialho@gmail.com>
2025-09-22 16:11:44 -0300

    ajuste de projeto

--- src/data/doctors.js
+++ src/data/doctors.js
@@ -11,4 +11,52 @@ export const doctors = [
     specialty: 'Fisioterapeuta',
     image: 'doctors/img-roberto.png',
   },
+  {
+    id: 3,
+    name: 'Ft. Jessica Teixeira',
+    specialty: 'Fisioterapeuta',
+    image: 'doctors/img-jessica.jpg',
+  },
+  {
+    id: 4,
+    name: 'Ft. Bruno Jucá',
+    specialty: 'Fisioterapeuta',
+    image: 'doctors/img-bruno.jpg',
+  },
+  {
+    id: 5,
+    name: 'Ft. Ana Laís Cidade',
+    specialty: 'Fisioterapeuta',
+    image: 'doctors/img-ana.jpg',
+  },
+  {
+    id: 6,
+    name: 'Ft. João Vitor Fernandes',
+    specialty: 'Fisioterapeuta',
+    image: 'doctors/img-joao-vitor-fernandes.jpg',
+  },
+  {
+    id: 7,
+    name: 'Ft. João Victor Barreto',
+    specialty: 'Fisioterapeuta',
+    image: 'doctors/img-joao-vitor-barreto.jpg',
+  },
+  {
+    id: 8,
+    name: 'Ft. Leandro Barreto',
+    specialty: 'Fisioterapeuta',
+    image: 'doctors/img-leandro-barreto.jpg',
+  },
+  {
+    id: 9,
+    name: 'Ft. Felipe Aragão',
+    specialty: 'Fisioterapeuta',
+    image: 'doctors/img-felipe-aragao.jpg',
+  },
+  {
+    id: 10,
+    name: 'Ft. João Otávio Araújo',
+    specialty: 'Fisioterapeuta',
+    image: 'doctors/img-joao-otavio.jpg',
+  },
 ];
